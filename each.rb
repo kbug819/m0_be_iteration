dd7ede97619e35e85c825a0151aa733ab0662749
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-05-16 23:37:40 -0500

    Initial commit

--- each.rb
+++ each.rb
@@ -6,6 +6,8 @@
 # Use the each method to print out a complete sentence stating the weather using the Array below:
 weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
+weather_options.each do |weather_option
+
 # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
